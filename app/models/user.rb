66290e85bf20e6b5d73ca553f528502cd0e2871c
chiharu05 <2102ct@gmail.com>
2021-12-21 17:50:39 +0900

    アソシエーションの記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :items
+  has_many :purchase_histories
+
   validates :encrypted_password, presence: true
   validates :family_name, presence: true
   validates :first_name, presence: true
