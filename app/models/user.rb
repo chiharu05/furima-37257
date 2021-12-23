75cf0a41c6747978a06cf93be42d0a67e49e3821
chiharu05 <2102ct@gmail.com>
2021-12-23 19:23:30 +0900

    モデルの単体テストコードの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,18 @@ class User < ApplicationRecord
   has_many :items
   #has_many :purchase_histories
 
-  validates :family_name, presence: true
-  validates :first_name, presence: true
-  validates :family_name_kana, presence: true
-  validates :first_name_kana, presence: true
-  validates :date_of_birth, presence: true
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates :password, format: { with: PASSWORD_REGEX }
+  with_options presence: true do
+    validates :nickname
+    validates :date_of_birth
+    with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/.freeze} do
+        validates :last_name
+        validates :first_name
+    end 
+    with_options format: { with: /\A[ァ-ンァ-ヶ一-龥々ー]+\z/.freeze} do
+      validates :last_name_kana
+      validates :first_name_kana
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -16,9 +16,9 @@ ActiveRecord::Schema.define(version: 2021_12_21_072342) do
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "nickname", null: false
-    t.string "family_name", null: false
+    t.string "last_name", null: false
     t.string "first_name", null: false
-    t.string "family_name_kana", null: false
+    t.string "last_name_kana", null: false
     t.string "first_name_kana", null: false
     t.date "date_of_birth", null: false
     t.string "reset_password_token"
