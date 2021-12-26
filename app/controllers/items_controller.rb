97e41fa550031226add181cd9be12bb59af5027a
chiharu05 <2102ct@gmail.com>
2021-12-27 00:23:54 +0900

    ActiveHashの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,8 @@
 class ItemsController < ApplicationController
-  #現段階で不使用 before_action :move_to_index, except: [:index, :show]
+  before_action :move_to_index, except: [:index, :show]
 
   def index
-    
+
   end
 
   def new
@@ -10,19 +10,24 @@ class ItemsController < ApplicationController
   end
 
   def create
-    Item.create(item_params)
+    @item = Item.create(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private
 
   def item_params
-    params.require(:item).permit(:name, :image, :text)
+    params.require(:item).permit(:items_name, :image, :items_profile, :category_id, :items_situation_id, :delivery_charge_id, :shipment_source_id, :price).merge(user_id: current_user.id)
   end
 
-  #現段階で不使用def move_to_index
-    #現段階で不使用 unless user_signed_in?
-      #現段階で不使用 redirect_to action: :index
-    #現段階で不使用 end
-  #現段階で不使用 end
+  def move_to_index
+    unless user_signed_in?
+    redirect_to action: :index
+    end
+  end
 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,7 @@
 class Item < ApplicationRecord
+  belongs_to :user
+  #has_one :purchase_history
+
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :items_situation
@@ -6,6 +9,7 @@ class Item < ApplicationRecord
   belongs_to :shipment_source
   belongs_to :shipping_date
 
-  belongs_to :user
-  #has_one :purchase_history
+  validates :items_name, :items_profile, :price,  presence: true
+  validates :category_id, :items_situation_id, :delivery_charge_id, :shipment_source_id, :shipping_date_id, numericality: { other_than: 1, message: "can't be blank" } 
+
 end
