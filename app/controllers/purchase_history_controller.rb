537b915cb5afe6f79c274863a1096f87c55a7c11
chiharu05 <2102ct@gmail.com>
2021-12-29 15:15:46 +0900

    商品一覧表示機能

---
+++ app/controllers/purchase_history_controller.rb
@@ -0,0 +1,18 @@
+class PurchaseHistoryController < ApplicationController
+  def index
+    @item = Item.find(params[:item_id])
+    @purchase_history = PurchaseHistory.new
+  end
+
+  def create
+    @item = Item.find(params[:item_id])
+    @purchase_history = @item.purchase_history.new(purchase_history_params)
+    @purchase_history.save
+  end
+
+  private
+
+  def purchase_history_params
+    params.require(:purchase_history).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+end
