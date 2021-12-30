271047d4c1b7342507b768505107980f6ec592c2
chiharu05 <2102ct@gmail.com>
2021-12-30 19:25:07 +0900

    リファクタリング

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
+  before_action :move_to_root_path, only: [:edit, :destroy]
 
   def index
     @items = Item.includes(:user).order('created_at DESC')
@@ -24,7 +25,6 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    redirect_to root_path unless @item.user_id == current_user.id
   end
 
   def update
@@ -37,7 +37,6 @@ class ItemsController < ApplicationController
 
   def destroy
     redirect_to root_path if @item.destroy
-    redirect_to root_path unless @item.user_id == current_user.id
   end
 
   private
@@ -50,5 +49,9 @@ class ItemsController < ApplicationController
 
   def set_item
     @item = Item.find(params[:id])
+  end
+
+  def move_to_root_path
+    redirect_to root_path unless @item.user_id == current_user.id
   end
 end
